7d2decfeadf866e08320ee6335d9237529cbef7d
yiliu <yi.liu@desy.de>
2016-09-22 10:47:55 +0200

    add nanotec motor device

--- source/device/thermal/include/LinkDef.hh
+++ source/device/thermal/include/LinkDef.hh
@@ -4,7 +4,15 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+
 #pragma link C++ class CameraIR;
-#pragma link C++ class MotorNanotec;
+// #pragma link C++ class MotorNanotec;
+#pragma link C++ namespace nanotec;
+#pragma link C++ defined_in namespace nanotec;
+
+#pragma link C++ class nanotec::NanotecMotor;
 
 #endif
